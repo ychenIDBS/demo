0738758021661585080501bc3687b288cfd93b40
Jon Stobbs <jstobbs@idbs.com>
2025-06-03 13:59:29 +0100

    NOTAG fix spelling

--- src/test/java/org/demo/RoverTest.java
+++ src/test/java/org/demo/RoverTest.java
@@ -10,12 +10,12 @@ public class RoverTest
 {
     @Test
     public void leftIfDirectionEastTest () {
-        Map<Integer, Integer> postion = new HashMap<>();
-        postion.put(3, 4);
+        Map<Integer, Integer> position = new HashMap<>();
+        position.put(3, 4);
 
-        Rover rover = new Rover(Direction.EAST, postion);
+        Rover rover = new Rover(Direction.EAST, position);
         rover.left();
         assertEquals(Direction.NORTH, rover.getDirection());
-        assertEquals(postion, rover.getPosition());
+        assertEquals(position, rover.getPosition());
     }
 }
